2425685f2051acb9ef32aa86dda086915648649b
Ramnath Sharma <ramsharma.axel@gmail.com>
2022-10-09 03:04:13 +0530

    js file update

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { addCacheItem, getCachedItem, isCached } from "./locationCaching.js";
-
 const API_KEY = "481a7c4ab04bc830b729294a0471613e";
 
 // NAME OF CITY
@@ -339,3 +337,43 @@ function getAverageRGB(imgEl) {
   // console.log(rgb);
   return rgb;
 }
+
+/* 
+cache 
+  - store weather info city wise in 1 object - 
+  this could be stored as 
+  "lan-long":
+    {
+      current forecast
+      hourly forecast
+      five day forcast
+      validity: 2 hours
+    }
+
+  clear cache on load if it's out of time
+
+
+*/
+const lc = localStorage;
+const DEFAULT_VALIDITY = 720000;
+
+const addCacheItem = (key, value) => {
+  lc.setItem(key, JSON.stringify({ value, validity: new Date() }));
+};
+
+const getCachedItem = (key) => {
+  let cachedItem = lc.getItem(key);
+  if (cachedItem) {
+    cachedItem = JSON.parse(cachedItem);
+    const itemValidity = new Date(cachedItem.validity);
+    if (new Date().getTime() - itemValidity.getTime() < DEFAULT_VALIDITY) {
+      return cachedItem.value;
+    }
+    lc.removeItem(key);
+  }
+  return null;
+};
+
+const isCached = (key) => {
+  return lc.getItem(key) ? true : false;
+};
